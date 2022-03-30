1474b2341b6814d33bb480e58c95840129047761
samandar-dev <samandarbohodirov01@gmail.com>
2022-03-30 13:02:31 +0500

    add

--- js/main-search.js
+++ js/main-search.js
@@ -11,35 +11,35 @@ const mainSearchText = document.querySelector('.msearch__text');
 const movListBox = document.querySelectorAll('.mov__list_box');
 
 formInput.addEventListener('keyup', formFunc);
-// selecCategor.addEventListener('click', formFunc);
-// selecYear1.addEventListener('click', formFunc);
 
-let formArr = []
+// let formArr = []
+// let formCat = []
 
 function formFunc() {
-  formSearchBtn.addEventListener('click', formFunc)
+  formSearchBtn.addEventListener('click', () => {
 
-  movListBox.forEach((items) => {
-    items.classList.add('d-none');
-  })
 
-  mainSearchBox.classList.remove('d-none');
-  mainSearchList.innerHTML = ''
+    movListBox.forEach((items) => {
+      items.classList.add('d-none');
+    })
 
-  let inputValueMain = formInput.value.toLocaleLowerCase();
-  // let categorss = selecCategor.value;
-  // let years =
-  let lisNull = true
+    mainSearchBox.classList.remove('d-none');
+    mainSearchList.innerHTML = ''
 
-  movies.forEach((item) => {
-    let movTitle = item.title;
-    let li = document.createElement('li');
-    li.className = "mov__item mitem";
+    let inputValueMain = formInput.value.toLocaleLowerCase();
+    // let categorss = selecCategor.value;
+    let lisNull = true
 
-    if (movTitle.toLocaleLowerCase().indexOf(inputValueMain) != -1 && formInput.value.length >= 1) {
-      console.log(selecYear1.value);
-      console.log('yesssss');
-      li.innerHTML = `
+
+    movies.forEach((item) => {
+      let movTitle = item.title;
+      let li = document.createElement('li');
+      li.className = "mov__item mitem";
+
+      if (movTitle.toLocaleLowerCase().indexOf(inputValueMain) != -1 && formInput.value.length >= 1 && selecCategor.value == 'All') {
+        console.log(selecYear1.value);
+        console.log('yesssss');
+        li.innerHTML = `
               <div class="mitem__box">
                 <div class="mitem__img-box">
                   <img class="mitem__img" src="${item.youtubePoster}" alt="imgs" id='${item.imdbId}'>
@@ -62,19 +62,20 @@ function formFunc() {
                 </div>
               </div>`;
 
-      mainSearchList.appendChild(li);
-      lisNull = false
-    }
-    else {
-      console.log('No');
+        mainSearchList.appendChild(li);
+        lisNull = false
+      }
+      else {
+        console.log('No');
+      }
+    })
+
+    if (lisNull == true && formInput.value.length >= 1) {
+      mainSearchText.classList.remove('d-none');
+    } else {
+      mainSearchText.classList.add('d-none');
     }
   })
-
-  if (lisNull == true && formInput.value.length >= 1) {
-    mainSearchText.classList.remove('d-none');
-  } else {
-    mainSearchText.classList.add('d-none');
-  }
 
   mainSearchCloseBtn.addEventListener('click', () => {
     movListBox.forEach((items) => {
--- js/my-movies.js
+++ js/my-movies.js
@@ -1,53 +1,60 @@
 const myList = document.querySelector('.my__list');
-let mItemBtn = document.querySelectorAll('.mitem__icon');
-let myCou = 1;
-let myArr = ['1']
 
-function myMoviesFun() {
-  mItemBtn.forEach((el) => {
-    if (myCou == 1) {
-      el.addEventListener('click', () => {
-        myCou = 2
-        let idd = el.id
-        myArr.push(idd);
-        for (let i = 0; i < movies.length; i++) {
-          let myLi = document.createElement('li');
-          myLi.className = "my__item";
-          if (idd == movies[i].imdbId) {
-            myLi.innerHTML = `
-            <div class="my__img-box">
-            <img class="my__img" src="${movies[i].youtubePoster}" alt="imgs" id='${movies[i].imdbId}'>
-              <button class="mitem__icon m-ite-act" id="${movies[i].imdbId}">
-                  <i class='bx bx-heart'></i>
-                </button>
-            </div>
-            <p class="my__tit">${movies[i].language}</p>
-            <h3 class="my__title">${movies[i].title}</h3>
-            <div class="my__price_box">
-              <div class="my__price d-flex align-items-center justify-content-between">
-                <div class="my__price-box">
-                  <span class="my__price-icon">
-                    <i class='bx bxs-star'></i>
-                  </span>
-                  <span class="my__rating">${movies[i].imdbRating}</span>
-                  </div>
-                  </div>
-                  <p class="my__janr my__tit">${movies[i].categories}</p>
-                  </div>`;
+heroSearch.addEventListener('keyup', heroFunc);
 
-            for (let j = 0; j < myArr.length; j++) {
-              if (myArr[j] != idd) {
-                el.classList.add('m-ite-act')
-                myList.appendChild(myLi);
-                console.log(myArr);
-                console.log(myArr[j]);
-                console.log(idd);
-              }
-            }
-          }
-        }
-      })
+// let formArr = []
+
+function heroFunc() {
+  heroIconSearch.addEventListener('click', () => {
+
+    mainSearchList.innerHTML = ''
+
+    let inputValueHero = heroSearch.value.toLocaleLowerCase();
+    let lisNull = true
+
+    movies.forEach((item) => {
+      let movTitle = item.title;
+      let li = document.createElement('li');
+      li.className = "mov__item mitem";
+
+      if (movTitle.toLocaleLowerCase().indexOf(inputValueHero) != -1 && heroSearch.value.length >= 1) {
+        console.log(inputValueHero);
+        console.log('yesssss');
+        li.innerHTML = `
+              <div class="mitem__box">
+                <div class="mitem__img-box">
+                  <img class="mitem__img" src="${item.youtubePoster}" alt="imgs" id='${item.imdbId}'>
+                  <button class="mitem__icon" id="${item.imdbId}">
+                    <i class='bx bx-heart'></i>
+                  </button>
+                </div>
+                <p class="mitem__tit">${item.language}, ${item.year} - Current</p>
+                <h3 class="mitem__title">${item.title}</h3>
+                <div class="mitem__price_box">
+                  <div class="mitem__price d-flex align-items-center justify-content-between">
+                    <div class="mitem__price-box">
+                      <span class="mitem__price-icon">
+                        <i class='bx bxs-star'></i>
+                      </span>
+                      <span class="mitem__rating">${item.imdbRating}</span>
+                    </div>
+                  </div>
+                  <p class="mitem__janr mitem__tit">${item.categories}</p>
+                </div>
+              </div>`;
+
+        heList.appendChild(li);
+        lisNull = false
+      }
+      else {
+        console.log('No');
+      }
+    })
+
+    if (lisNull == true && formInput.value.length >= 1) {
+      mainSearchText.classList.remove('d-none');
+    } else {
+      mainSearchText.classList.add('d-none');
     }
   })
 }
-myMoviesFun();
